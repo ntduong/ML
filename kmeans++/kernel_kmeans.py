53071c78f85d5284484264ecf2c80ef1bc72df40
duong nguyen <ntduong268@gmail.com>
2013-05-30 18:14:35 +0900

    updated kernel kmeans

--- kmeans++/kernel_kmeans.py
+++ kmeans++/kernel_kmeans.py
@@ -7,14 +7,9 @@ Email: nguyen@sg.cs.titech.ac.jp
 
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 from numpy.random import shuffle
 from sklearn.metrics.pairwise import euclidean_distances
-
-
-def gaussian_kernel(x, y, c):
-    """ K(x, y) = exp(-||x-y||^2 / (c^2)). """
-    return np.exp(-np.dot(x-y, x-y)/(c*c))
 
 def kernel_kmeans(X, k=2, c=0.6, n_iter=1000, tol=1e-10):
     """ Kernel k-means algorithm with Gaussian kernel K(x,y) = exp(-||x-y||^2 / (c*c))
@@ -30,11 +25,13 @@ def kernel_kmeans(X, k=2, c=0.6, n_iter=1000, tol=1e-10):
         Returns: 
             Set of clusters. Each cluster contains indices of samples belong to that cluster.
     """
-    
+        
     n, d = X.shape
     
     # First, randomly initialize cluster partition
     X_ind = np.arange(0,n)
+    
+    #np.random.seed(1)
     
     shuffle(X_ind)
     split_ind = np.arange(n/k, (n/k)*k, n/k)
@@ -47,7 +44,33 @@ def kernel_kmeans(X, k=2, c=0.6, n_iter=1000, tol=1e-10):
     sqD = euclidean_distances(X, X, squared=True)
     K = np.exp(-sqD/(c*c))
     
+    def obj_function(aClusters):
+        """ Compute the objective function value given a cluster configuration.
+            Used in convergence checking.
+            
+            Params:
+                aClusters: [[cluster_1], [cluster_2],..., [cluster_k]]
+                
+            Returns:
+                The value of objective function defined as:
+                sum_{j=1}^k sum_{x \in C_j} || psi(x) - mu_j ||^2
+        """
+        
+        obj_val = float(0)
+        for ci in range(k):
+            ci_size = len(aClusters[ci])
+            temp1 = sum([K[a,a] for a in aClusters[ci]])
+            temp2 = sum([K[a,b] for a in aClusters[ci] for b in aClusters[ci]]) * 1.0 / ci_size
+            obj_val += temp1 - temp2
+            
+        return obj_val
+                
+    
+    obj_val_list = []
+    
     for i in range(n_iter):
+        obj_val_list.append(obj_function(clusters))
+        
         tmp_clusters = [[] for _ in range(k)]
         
         for xi in range(n):
@@ -69,6 +92,9 @@ def kernel_kmeans(X, k=2, c=0.6, n_iter=1000, tol=1e-10):
             tmp_clusters[min_ci].append(xi)
         
         # Check if converge
+        
+        '''
+        # One way to check if converge or not
         # diff is sum of distance between k old and new centroids 
         diff = 0 
         for ci in range(k):
@@ -81,6 +107,35 @@ def kernel_kmeans(X, k=2, c=0.6, n_iter=1000, tol=1e-10):
             
         if diff <= tol: # break if converge
             break
+        '''
+        
+        '''
+        # Different way to check convergence: If the value of objective function is not unchanged
+        # or changed too little under some tolerance, then stop.
+        old_obj_val = obj_function(clusters)
+        new_obj_val = obj_function(tmp_clusters)
+        
+        obj_val_list.append(old_obj_val) # to see if the value of objective function decrease at each iteration
+        
+        if np.abs(new_obj_val-old_obj_val) <= tol: # convergence
+            break
+        '''
+        
+        # Simple convergence checking
+        is_converge = True
+        for ci in range(k):
+            old_ci_size = len(clusters[ci])
+            new_ci_size = len(tmp_clusters[ci])
+            if old_ci_size != new_ci_size:
+                is_converge = False
+                break
+            for xi in clusters[ci]:
+                if xi not in tmp_clusters[ci]:
+                    is_converge = False
+                    break
+            
+        if is_converge:
+            break
         
         # Update new clusters
         for ci in range(k):
@@ -88,7 +143,7 @@ def kernel_kmeans(X, k=2, c=0.6, n_iter=1000, tol=1e-10):
         
         
     assert len(clusters) == k, "We need k clusters!"
-    return clusters
+    return clusters, obj_val_list
         
 def loadData(fname):
     X = []
@@ -100,22 +155,31 @@ def loadData(fname):
     X = np.array(X, dtype='float64')
     return X
 
-def with_dataset(fname='2d-6.txt',figname='2d-6.png'):
-    X = loadData(fname)
+def with_dataset(c, dat_fname, fig_name, obj_fname):
+    X = loadData(dat_fname)
     n_clusters = 2
-    X_cluster = kernel_kmeans(X, k=n_clusters, c=0.1, n_iter=1000, tol=1e-7)
+    n_iter = 100
+    X_cluster, obj_val_list = kernel_kmeans(X, k=n_clusters, c=c, n_iter=n_iter, tol=1e-7)
     
     cs = ['r', 'b']
     ms = ['o', 'x']
-    fig = plt.figure()
+    fig = plt.figure(1)
     fig.clf()
     for i in range(n_clusters):
         plt.plot(X[X_cluster[i], 0], X[X_cluster[i], 1], cs[i]+ms[i])
     
     plt.axis('off')
-    plt.savefig(figname)
-    plt.show()
+    plt.savefig(fig_name)
+    
+    fig = plt.figure(2)
+    fig.clf()
+    
+    plt.plot(range(1, len(obj_val_list)+1), obj_val_list, 'b-', lw=1.5)
+    plt.xlabel('Iteration')
+    plt.ylabel('Objective function')
+    plt.savefig(obj_fname)
+    #plt.show()
     
 if __name__ == '__main__':
-    with_dataset(fname='2d-7.txt', figname='2d-7_c_01.png')
-    
+    with_dataset(0.6, '2d-6/2d-6.txt', '2d-6/2d-6_c_06.png', '2d-6/2d-6_obj_c_06.png')
+    #with_dataset(0.3, '2d-7/2d-7.txt', '2d-7/2d-7_c_03.png', '2d-7/2d-7_obj_c_03.png')
